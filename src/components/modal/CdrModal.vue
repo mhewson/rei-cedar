8d8d2d8dd5c14336ab9a64e1f24db81ca23b6ed0
Matt English <menglis@rei.com>
2018-02-02 09:54:50 -0800

    Fix eslint errors

--- src/components/modal/CdrModal.vue
+++ src/components/modal/CdrModal.vue
@@ -1,5 +1,29 @@
 <template>
-  
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+          <cdr-row>
+            <cdr-col span="12">
+              <cdr-button modifier="sm">Close</cdr-button>
+            </cdr-col>
+            <cdr-col span="12">
+              <slot name="header"/>
+            </cdr-col>
+            <cdr-col span="12">
+              <slot name="body"/>
+            </cdr-col>
+            <cdr-col span="12">
+              <slot name="footer"/>
+            </cdr-col>
+            <cdr-col span="12">
+              <slot name="extra"/>
+            </cdr-col>
+          </cdr-row>
+        </div>
+      </div>
+    </div>
+  </transition>
 </template>
 
 <script>
--- src/components/modal/examples/Modal.vue
+++ src/components/modal/examples/Modal.vue
@@ -1,29 +1,31 @@
 <template>
-        <transition name="modal">
-            <div class="modal-mask">
-                <div class="modal-wrapper">
-                    <div class="modal-container">
-                        <cdr-row>
-                            <cdr-col span="12">
-                                <cdr-button modifier="sm">Close</cdr-button>
-                            </cdr-col>
-                            <cdr-col span="12">
-                                <slot name="header"></slot>
-                            </cdr-col>
-                            <cdr-col span="12">
-                                <slot name="body"></slot>
-                            </cdr-col>
-                            <cdr-col span="12">
-                                <slot name="footer"></slot>
-                            </cdr-col>
-                            <cdr-col span="12">
-                                <slot name="extra"></slot>
-                            </cdr-col>
-                        </cdr-row>
-                    </div>
+    <transition name="modal">
+        <div class="modal-mask">
+            <div class="modal-wrapper">
+                <div class="modal-container">
+                    <cdr-row>
+                        <cdr-col span="12">
+                            <cdr-button modifier="sm">Close</cdr-button>
+                        </cdr-col>
+                        <cdr-col span="12">
+                            <slot name="header">TODO: // show how to import data when invoking modal</slot>
+                        </cdr-col>
+                        <cdr-col span="12">
+                            <slot name="body"></slot>
+                        </cdr-col>
+                        <cdr-col span="12">
+                            <slot name="footer">
+                                <p>PreExisting content</p>
+                            </slot>
+                        </cdr-col>
+                        <cdr-col span="12">
+                            <slot name="extra"></slot>
+                        </cdr-col>
+                    </cdr-row>
                 </div>
             </div>
-        </transition>
+        </div>
+    </transition>
 </template>
 
 <script>
@@ -33,60 +35,5 @@ export default {
 </script>
 
 <style>
-    .modal-mask {
-        position: fixed;
-        z-index: 9998;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, .5);
-        display: table;
-        transition: opacity .3s ease;
-    }
-
-    .modal-wrapper {
-        display: table-cell;
-        vertical-align: middle;
-    }
-
-    .modal-container {
-        max-width: 500px;
-        max-height: 600px;
-        overflow: scroll;
-        margin: 0px auto;
-        padding: 20px 30px;
-        background-color: #fff;
-        border-radius: 2px;
-        box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
-        transition: all .3s ease;
-        font-family: Helvetica, Arial, sans-serif;
-    }
-
-    .modal-body {
-        margin: 20px 0;
-    }
     
-    /*
-    * The following styles are auto-applied to elements with
-    * transition="modal" when their visibility is toggled
-    * by Vue.js.
-    *
-    * You can easily play with the modal transition by editing
-    * these styles.
-    */
-
-    .modal-enter {
-        opacity: 0;
-    }
-
-    .modal-leave-active {
-        opacity: 0;
-    }
-
-    .modal-enter .modal-container,
-    .modal-leave-active .modal-container {
-        -webkit-transform: scale(1.1);
-        transform: scale(1.1);
-    }
 </style>
