795cea3f79ba273b9e794e97ddc5c5ce012b3061
Darin Trimillos <darin@trimillos.com>
2018-04-16 17:42:36 -0700

    WIP: added component scaffolding
    affects: @rei/cdr-icon-button

---
+++ src/components/iconButton/CdrIconButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- disable lint errors on line length in template -->
+  <!-- eslint-disable max-len -->
+</template>
+
+<script>
+export default {
+  name: 'CdrIconButton',
+};
+</script>
+
+<style>
+  @import '../../css/settings/_index.pcss';
+  @import './styles/CdrIconButton.pcss';
+</style>
